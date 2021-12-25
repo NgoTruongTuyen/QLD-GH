b58d423dd963e3ce54f1baccd526e3e73c45ed42
phamngocanh19127098 <npham4533@gmail.com>
2021-12-25 18:12:17 +0700

    rename file

--- src/DT_DangKyTK1.java
+++ src/DT_DangKyTK1.java
@@ -20,12 +20,12 @@ import javax.swing.table.DefaultTableModel;
  *
  * @author zerotus
  */
-public class DT_DangKyTK extends javax.swing.JFrame {
+public class DT_DangKyTK1 extends javax.swing.JFrame {
     Connection connection;
     /**
      * Creates new form ManagerList
      */
-    public DT_DangKyTK() {
+    public DT_DangKyTK1() {
         initComponents();
     }
     public String getMaDT(){
@@ -317,14 +317,18 @@ public class DT_DangKyTK extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(DT_DangKyTK.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DT_DangKyTK1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(DT_DangKyTK.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DT_DangKyTK1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(DT_DangKyTK.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DT_DangKyTK1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(DT_DangKyTK.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DT_DangKyTK1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
@@ -333,7 +337,7 @@ public class DT_DangKyTK extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new DT_DangKyTK().setVisible(true);
+                new DT_DangKyTK1().setVisible(true);
             }
         });
     }
